5c6311499650b57a2dc77e8d63264f70805efcb8
Mateus Dias <mfd0021@gmail.com>
2024-09-10 12:14:02 -0300

    completo pg

--- Back/database.js
+++ Back/database.js
@@ -1,7 +1,10 @@
-const { Pool } = require('pg'); // Substituir por PostgreSQL
+const mysql = require('mysql2');
 
-const pool = new Pool({
-    connectionString: 'postgresql://user_db:616SgPRstweBDis3Rh77kWMIfRU9oy6d@dpg-crg5vut6l47c73dsfh1g-a/veiculos_67lu'
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'dickin',
+    password: 'root123'
 });
 
-module.exports = pool;
+module.exports = pool.promise();
